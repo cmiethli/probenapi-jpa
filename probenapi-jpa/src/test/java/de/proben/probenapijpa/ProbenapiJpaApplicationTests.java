cc5a9b57eee92945c292e9a73684734b9b9dce69
cmiethli <christian.miethling86@gmail.com>
2020-05-06 17:04:33 +0200

    init

---
+++ probenapi-jpa/src/test/java/de/proben/probenapijpa/ProbenapiJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.proben.probenapijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbenapiJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
