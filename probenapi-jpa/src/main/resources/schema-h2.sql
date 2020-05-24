075f1d057ddb097db763f239a967058e17514cf7
cmiethli <christian.miethling86@gmail.com>
2020-05-24 20:44:02 +0200

    schema-h2.sql rausgenommen und init Schema in url eingefuegt >>wie MySQL

--- probenapi-jpa/src/main/resources/schema-h2.sql
+++
@@ -1,3 +0,0 @@
--- CREATE DATABASE IF NOT EXISTS: H2 is supposed to create the schema (with correct name) automatically but it doesnt!
--- wir muessen es machen!
-CREATE SCHEMA IF NOT EXISTS @dbName@;
