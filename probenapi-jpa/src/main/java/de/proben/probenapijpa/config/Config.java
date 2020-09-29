6e1a381791788f089e7184e78a91e9fdb481608d
Christian.Miethling <c.miethling@qualitype.de>
2020-09-29 13:39:51 +0200

    h2Db an

--- probenapi-jpa/src/main/java/de/proben/probenapijpa/config/Config.java
+++ probenapi-jpa/src/main/java/de/proben/probenapijpa/config/Config.java
@@ -7,20 +7,20 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 // alle Componenten finden
 @ComponentScan(basePackages = { "de.proben.probenapijpa.api" })
-@PropertySource({
-	"classpath:persistence-${persistenceTarget:mysql}.properties" })
-//@PropertySource({ "classpath:persistence-${persistenceTarget:h2}.properties" })
+// @PropertySource({
+// "classpath:persistence-${persistenceTarget:mysql}.properties" })
+@PropertySource({ "classpath:persistence-${persistenceTarget:h2}.properties" })
 public class Config {
 
-//// hier waere Config Factory von ProbenVerwalten
-//// nicht mehr notwendig da Klassen als Component annotiert
-//	@Bean("db")
-//	public ProbenVerwalten a() {
-//		return ProbenVerwaltenFactory.getInstance(Instance.DB);
-//	}
-//
-//	@Bean("inMem")
-//	public ProbenVerwalten b() {
-//		return ProbenVerwaltenFactory.getInstance(Instance.IN_MEM);
-//	}
+	//// hier waere Config Factory von ProbenVerwalten
+	//// nicht mehr notwendig da Klassen als Component annotiert
+	// @Bean("db")
+	// public ProbenVerwalten a() {
+	// return ProbenVerwaltenFactory.getInstance(Instance.DB);
+	// }
+	//
+	// @Bean("inMem")
+	// public ProbenVerwalten b() {
+	// return ProbenVerwaltenFactory.getInstance(Instance.IN_MEM);
+	// }
 }
